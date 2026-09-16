485ccaf2ef06a0372171a3fffbef5d7ff681b18b
nonkun12 <54180311+nonkun12@users.noreply.github.com>
2026-09-16 21:45:11 +0900

    feat: export agent handoff coordinator

--- core/__init__.py
+++ core/__init__.py
@@ -6,6 +6,7 @@ from .agent_versioning import AgentVersion, AgentVersionRegistry
 from .agent_maintenance import AgentMaintenance, MaintenanceFinding, MaintenanceSeverity
 from .agent_governance import AgentGovernance, AgentGovernanceRecord
 from .agent_handoff import AgentHandoff, HandoffStatus
+from .agent_handoff_coordinator import AgentHandoffCoordinator, HandoffDecision
 from .creator_critic import CriticAgent, CriticEvaluation, CreatorAgent, CreatorCriticLoop, DuelResult, ImprovementCandidate
 from .execution_pipeline import DefaultPlanner, ExecutionResult, Manager, WorkPlan, WorkRequest
 from .gateway import AIGateway, AIRequest, AIResponse
@@ -32,7 +33,7 @@ __all__ = [
     "AgentLifecycle", "AgentSpec", "AgentSpecRegistry",
     "AgentVersion", "AgentVersionRegistry", "AgentMaintenance",
     "MaintenanceFinding", "MaintenanceSeverity", "AgentGovernance", "AgentGovernanceRecord",
-    "AgentHandoff", "HandoffStatus",
+    "AgentHandoff", "HandoffStatus", "AgentHandoffCoordinator", "HandoffDecision",
     "CreatorAgent", "CriticAgent", "CreatorCriticLoop", "ImprovementCandidate",
     "CriticEvaluation", "DuelResult", "Manager", "WorkRequest", "WorkPlan",
     "ExecutionResult", "DefaultPlanner", "TaskMode", "TaskClassification",
